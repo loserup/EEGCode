1b5e85459061839b4c1e8448255386a8b9b3f8a2
Single Long <623962644@qq.com>
2018-08-08 18:17:12 +0800

    新的同步按键
    通过while循环保证在按下虚拟F3键前使脑电软件在最前面

--- OnlineTest/SyncKey/SyncKey/SyncKey/Program.cs
+++ OnlineTest/SyncKey/SyncKey/SyncKey/Program.cs
@@ -11,8 +11,8 @@ namespace SyncKey
 {
     class Program
     {
-        [System.Runtime.InteropServices.DllImport("user32.dll", EntryPoint = "SendInput", SetLastError = true)]
-        private static extern uint SendInput(uint numberOfInputs, INPUT[] inputs, int sizeOfInputStructure);
+        [DllImport("user32.dll", EntryPoint = "SendMessage")]
+        private static extern int SendMessage(IntPtr hwnd, int msg, int wParam, int lParam);
 
         [DllImport("User32.dll", EntryPoint = "FindWindow")]
         private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
@@ -21,95 +21,24 @@ namespace SyncKey
         private static extern bool SetForegroundWindow(IntPtr hWnd);
 
         [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
         private static extern short GetAsyncKeyState(int vKey);
 
         // 虚拟按键码：https://docs.microsoft.com/zh-cn/windows/desktop/inputdev/virtual-key-codes
         private const int VK_LBUTTON = 0x01; // 鼠标左键
         private const int VK_F3 = 0x72; // F3键-脑电软件打标快捷键
-        private const int VK_RBUTTON = 0x02;
-        //private const int VK_TAB = 0x09;
-        //private const int VK_WIN = 0x5B;
+        //private const int VK_RBUTTON = 0x02;
         private static bool pressed = false;
 
-        [Flags()]
-        private enum InputType
-        {
-            INPUT_MOUSE = 0,
-            INPUT_KEYBOARD = 1,
-            INPUT_HARDWARE = 2,
-        }
-
-        [Flags()]
-        private enum KEYEVENTF
-        {
-            EXTENDEDKEY = 0x0001,
-            KEYUP = 0x0002,
-            UNICODE = 0x0004,
-            SCANCODE = 0x0008,
-        }
-
-        /// <summary>
-        /// 请特别注意这里！！！！！x64平台使用8个字节偏移，x86平台使用4个字节的偏移！！！
-        /// </summary>
-        [StructLayout(LayoutKind.Explicit)]
-        private struct INPUT
-        {
-            [FieldOffset(0)]
-            public Int32 type;//0-MOUSEINPUT;1-KEYBDINPUT;2-HARDWAREINPUT     
-            [FieldOffset(8)]
-            public KEYBDINPUT ki;
-            [FieldOffset(8)]
-            public MOUSEINPUT mi;
-            [FieldOffset(8)]
-            public HARDWAREINPUT hi;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct KEYBDINPUT
-        {
-            public Int16 wVk;
-            public Int16 wScan;
-            public Int32 dwFlags;
-            public Int32 time;
-            public IntPtr dwExtraInfo;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct MOUSEINPUT
-        {
-            public Int32 dx;
-            public Int32 dy;
-            public Int32 mouseData;
-            public Int32 dwFlags;
-            public Int32 time;
-            public IntPtr dwExtraInfo;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct HARDWAREINPUT
-        {
-            public Int32 uMsg;
-            public Int16 wParamL;
-            public Int16 wParamH;
-        }
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_KEYUP = 0x0101;
 
         static void Main(string[] args)
         {
-            UInt32 result = 0;
-            INPUT input_key_F3_down = new INPUT();
-            INPUT input_key_F3_up = new INPUT();
-
             ////这里是当需要针对某个应用的时候可以先获取这个应用的句柄，接着就可以对这个应用发送想要的键盘操作
             IntPtr getwnd = FindWindow(null, "ActiView703-Lores.vi"); // 捕捉脑电软件进程
-            //IntPtr getwnd = GetDesktopWindow();
-
-
-            input_key_F3_down.type = input_key_F3_down.type = (int)InputType.INPUT_KEYBOARD;
-            input_key_F3_down.ki.wVk = input_key_F3_down.ki.wVk = VK_F3;
-            input_key_F3_down.ki.dwFlags = 0;
-            input_key_F3_up.ki.dwFlags = (Int32)KEYEVENTF.KEYUP;
-
-            INPUT[] inputs = new INPUT[] { input_key_F3_down, input_key_F3_up};
 
             while (true)
             {
@@ -125,10 +54,8 @@ namespace SyncKey
                             Console.Write("-1\n");
                             return;
                         }
-                        // 在脑电软件外部点击左键后会有那么一瞬间脑电软件非当前进程，造成虚拟F3按键没有在软件内按下，所以这里做个延时，延时越长成功率越大，但对同步率的影响也越大
-                        System.Threading.Thread.Sleep(200); 
-                        result = SendInput(2, inputs, Marshal.SizeOf(typeof(INPUT)));
-
+                        while (GetForegroundWindow() != getwnd) { }
+                        SendMessage(getwnd, WM_KEYDOWN, VK_F3, 0);                       
                         pressed = false;
                     }
                 }
